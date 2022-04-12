e5eda8f91414e94a292ac34d810331e364042251
sweta shree <sweta.shree@my.jcu.edu.au>
2022-04-12 12:28:43 +0530

    Add program list warmup

---
+++ lists_warmup.py
@@ -0,0 +1,64 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# if 1 in marks:
+#     print("True")
+# else:
+#     print("False")
+
+
+"""
+Q: numbers[0] 
+A: [3]
+
+Q: numbers[-1] 
+A: [2]
+
+Q: numbers[3]
+A: [1]
+
+Q: numbers[:-1]
+A: [3,1,4,2,5,9]
+
+Q: numbers[3:4]
+A: [1]
+
+Q: 5 in numbers
+A: True
+
+Q: 7 in numbers
+A: False
+
+Q: "3" in numbers
+A: False
+
+Q: numbers + [6, 5, 3]
+A: 
+"""
+
+numbers + [6, 5, 3]
+
+
+"""
+Q:1 Change the first element of numbers to "ten" (the string, not the number 10)
+"""
+# numbers[0] = "ten"
+# print(numbers)
+
+"""
+Q:2 Change the last element of numbers to 1
+"""
+# numbers[-1] = 1
+# print(numbers)
+
+"""
+Q:3 Get all the elements from numbers except the first two (slice)
+"""
+# print(numbers[2:])
+
+"""
+Q:4 Check if 9 is an element of numbers
+"""
+# if 9 in numbers:
+#     print("True")
+# else:
+#     print("False")
